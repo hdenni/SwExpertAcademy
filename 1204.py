9e65e734712c60f19574defdf855d3184d642500
hdenni <44808373+hdenni@users.noreply.github.com>
2019-01-07 19:24:14 +0900

    Add files via upload

---
+++ 1204.py
@@ -0,0 +1,23 @@
+#1204 - 최빈값 구하기
+
+testcase = int(input())
+
+for i in range(testcase):
+    tc = int(input())
+    dic = {}
+    scores=input()
+    score_list = scores.split()
+    for j in score_list:
+        dic[j] = dic.get(j, 0)+1
+
+    a, b = max(zip(dic.values(), dic.keys()))
+    sort_list = sorted(zip(dic.values(), dic.keys()))
+
+    new_dic = []
+    for key, value in dic.items():
+        if value == a:
+            new_dic.append(key)
+
+    result = int(max(new_dic))
+    
+    print("#%d %d"%(tc,result))
